740d0793ae9223c72c280e74f9f4a5cc3d2c290b
RobertCPatrona <robert.patrona246@gmail.com>
2020-11-29 11:48:48 +0200

    URLfix

--- public/js/app.js
+++ public/js/app.js
@@ -49884,8 +49884,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Code\Instagram-Clone\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Code\Instagram-Clone\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Code\Pictures-Posting-App\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Code\Pictures-Posting-App\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -36,7 +36,7 @@
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
-            <div><a href="#">{{ $user->profile->url }}</a></div>
+            <div><a href="{{ $user->profile->url }}" target="_blank">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
